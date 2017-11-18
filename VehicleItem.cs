c8ac92a1df372ad0b195fa488bd3ef80c9023075
Edsparr <fr0zensweden@gmail.com>
2017-11-18 11:16:17 +0100

    Release!

---
+++ VehicleItem.cs
@@ -0,0 +1,24 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Edsparr.VehicleCustomizer
+{
+    public class VehicleItem
+    {
+        public ushort vehicleID;
+        public byte increaseWidth;
+        public byte increaseHeight;
+        public decimal cost;
+        public VehicleItem(ushort vehicleID, byte increaseWidth, byte increaseHeight, decimal cost)
+        {
+            this.vehicleID = vehicleID;
+            this.increaseWidth = increaseWidth;
+            this.increaseHeight = increaseHeight;
+            this.cost = cost;
+        }
+        public VehicleItem() { }
+    }
+}
