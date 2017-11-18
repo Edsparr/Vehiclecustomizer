ec06a8534764dd8fcd635b2765f7d7a6bea24585
Edsparr <fr0zensweden@gmail.com>
2017-11-18 02:18:59 +0100

    Release

---
+++ CommandBuyspace.cs
@@ -0,0 +1,149 @@
+﻿using Rocket.API;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Text;
+using Rocket.API.Extensions;
+using Rocket.Unturned.Permissions;
+using Rocket.Core;
+using fr34kyn01535.Uconomy;
+
+namespace Edsparr.VehicleCustomizer
+{
+    public class CommandBuyspace : IRocketCommand
+    {
+        public VehicleCustomizerPlugin plugin = VehicleCustomizerPlugin.Instance;
+        #region Declarations
+
+        public bool AllowFromConsole
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public List<string> Permissions
+        {
+            get
+            {
+                return new List<string>()
+                {
+                    "buyspace"
+                };
+            }
+        }
+
+        public AllowedCaller AllowedCaller
+        {
+            get
+            {
+                return AllowedCaller.Player;
+            }
+        }
+
+        public bool RunFromConsole
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "buyspace";
+            }
+        }
+
+        public string Syntax
+        {
+            get
+            {
+                return "/buyspace";
+            }
+        }
+
+        public string Help
+        {
+            get
+            {
+                return "You buy extra trunkstorage for your car!";
+            }
+        }
+
+        public List<string> Aliases
+        {
+            get
+            {
+                return new List<string>();
+            }
+        }
+
+        #endregion
+
+
+
+
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+            if(player.CurrentVehicle == null)
+            {
+                UnturnedChat.Say(player, VehicleCustomizerPlugin.Instance.Translate("not_in_car"), Color.red);
+                return;
+            }
+            if(player.CurrentVehicle.trunkItems == null)
+            {
+                UnturnedChat.Say(player, VehicleCustomizerPlugin.Instance.Translate("no_trunk_space"), Color.red);
+                return;
+            }
+            byte newwidth = 0;
+            byte newwheight = 0;
+            decimal cost = 0;
+            var overridenitem = VehicleCustomizerPlugin.Instance.Configuration.Instance.OverridenVehicles.Find(c => (c.vehicleID == player.CurrentVehicle.asset.id));
+            if (overridenitem != null) //should have special price
+            {
+                int nw = player.CurrentVehicle.trunkItems.width + overridenitem.increaseWidth;
+                newwidth = (byte)nw;
+                int nh = player.CurrentVehicle.trunkItems.height + overridenitem.increaseHeight;
+                newwheight = (byte)nh;
+                cost = overridenitem.cost;
+            }
+            else //normal price
+            {
+                cost = VehicleCustomizerPlugin.Instance.Configuration.Instance.normalCost;
+                int nw  = player.CurrentVehicle.trunkItems.width + VehicleCustomizerPlugin.Instance.Configuration.Instance.increaseWidth;
+                newwidth = (byte)nw;
+                int nh = player.CurrentVehicle.trunkItems.height + VehicleCustomizerPlugin.Instance.Configuration.Instance.increaseHeight;
+                newwheight = (byte)nh;
+            }
+            if(player.CurrentVehicle.trunkItems.width >= 255 || player.CurrentVehicle.trunkItems.height >= 254)
+            {
+                UnturnedChat.Say(VehicleCustomizerPlugin.Instance.Translate("already_maxed"), Color.red);
+                return;
+            }
+            if(cost > Uconomy.Instance.Database.GetBalance(player.CSteamID.ToString()))
+            {
+                UnturnedChat.Say(player, VehicleCustomizerPlugin.Instance.Translate("cant_afford", cost), Color.red);
+                return;
+            }
+            player.CurrentVehicle.trunkItems.resize(newwidth, newwheight);
+            Uconomy.Instance.Database.IncreaseBalance(player.CSteamID.ToString(), -cost);
+            UnturnedChat.Say(player, VehicleCustomizerPlugin.Instance.Translate("succesfully_bought", newwidth.ToString(), newwheight.ToString(), cost.ToString()), Color.yellow);
+        }
+    }
+}
+
+
---
+++ Plugin.cs
@@ -0,0 +1,86 @@
+﻿using Rocket.API.Collections;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using Steamworks;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using UnityEngine;
+using SDG.Unturned;
+using fr34kyn01535.Uconomy;
+using System.Text;
+using Rocket.Unturned.Events;
+using Rocket.API;
+using Rocket.Unturned.Enumerations;
+using Rocket.Unturned.Permissions;
+
+namespace Edsparr.VehicleCustomizer
+{
+    public class VehicleCustomizerPlugin : RocketPlugin<VehicleCustomizerConfiguration>
+    {
+
+        public Vector3 pos;
+
+        public static string version = "1.0";
+        public static VehicleCustomizerPlugin Instance;
+
+        public override TranslationList DefaultTranslations
+        {
+            get
+            {
+                return new TranslationList(){
+                {"not_in_car","You're not in a car!"},
+                {"already_maxed","You have already maded the storage as big as possible!"},
+                {"cant_afford","You can't afford to buy extra space for this vehicle!"},
+                {"succesfully_bought","You succesfully bought {0} in extra width and {1} extra in height for {2]!"},
+                {"no_trunk_space","This car doesn't have any portable storage unit!"},
+                {"invalid_syntax_unban","/punban <steamId>"},
+                {"succesfully_unbanned","You paid {0} {1} {2}"}
+                };
+            }
+        }
+
+        protected override void Load()
+        {
+            VehicleCustomizerPlugin.Instance = this;
+            loadAllVehicles();
+        }
+        private void loadAllVehicles()
+        {
+            foreach(var item in Configuration.Instance.DataStorageIGNORE)
+            {
+                var vehicle = VehicleManager.getVehicle(item.instanceID);
+                if (vehicle == null) continue;
+                vehicle.trunkItems.resize(item.width, item.height);
+            }
+            Configuration.Instance.DataStorageIGNORE.Clear();
+            Configuration.Save();
+        }
+        protected override void Unload()
+        {
+            saveAllVehicles();
+        }
+
+        private void saveAllVehicles()
+        {
+            Configuration.Instance.DataStorageIGNORE.Clear();
+            Configuration.Save();
+            foreach (var vehicle in VehicleManager.vehicles)
+            {
+                Configuration.Instance.DataStorageIGNORE.Add(new EachVehicle(vehicle.instanceID, vehicle.trunkItems.width, vehicle.trunkItems.height));
+            }
+            Configuration.Save();
+        }
+    }
+
+}
+
+
+
+
+
+
+
