8934bcef740e96c1c07e2c46c998ea6fffa95733
Edsparr <fr0zensweden@gmail.com>
2017-11-18 01:52:55 +0100

    Release

---
+++ eachVehicleItem.cs
@@ -0,0 +1,23 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Edsparr.VehicleCustomizer
+{
+    public class EachVehicle
+    {
+        public uint instanceID;
+        public byte width;
+        public byte height;
+
+        public EachVehicle(uint instanceID, byte width, byte height)
+        {
+            this.instanceID = instanceID;
+            this.width = width;
+            this.height = height;
+        }
+        public EachVehicle() { }
+    }
+}
