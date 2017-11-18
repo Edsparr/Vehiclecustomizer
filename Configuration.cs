78ad53af77d44ea6c2b1aca4beab942e597189ac
Edsparr <fr0zensweden@gmail.com>
2017-11-18 01:18:52 +0100

    Release!

---
+++ Configuration.cs
@@ -0,0 +1,25 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Edsparr.VehicleCustomizer
+{
+    public class VehicleCustomizerConfiguration : IRocketPluginConfiguration
+    {
+        public List<VehicleItem> OverridenVehicles = new List<VehicleItem>();
+        public ushort normalCost;
+        public byte increaseWidth;
+        public byte increaseHeight;
+        public List<EachVehicle> DataStorageIGNORE = new List<EachVehicle>();
+        public void LoadDefaults()
+        {
+            OverridenVehicles.Add(new VehicleItem(67, 10, 10, 500));
+            normalCost = 250;
+            increaseHeight = 5;
+            increaseWidth = 5;
+            DataStorageIGNORE = null;
+        }
+    }
+}
